b5b4f8a04c9f7010ca6b5486afdd1aaf478d165f
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-04 17:33:02 +0200

    exercise-complete
    js-simon

--- js/main.js
+++ js/main.js
@@ -2,10 +2,9 @@
 function randomNumbers(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
-
 //2. make some variables for the randomize numbers
-var min = 1;
-var max = 100;
+let min = 1;
+let max = 100;
 //3. make a cicle to create 5 randomize numbers for the utent
 var arrayNumbers = [];
 while (arrayNumbers.length < 5) {
@@ -17,12 +16,14 @@ while (arrayNumbers.length < 5) {
 console.log(arrayNumbers);
 //4. An alert appears to show 5 numbers to the utent
 alert(arrayNumbers);
-// 5. make the utent number variable and an array with the numbers inside
-var arrayUtentNumbers = [];
-//6. 5 prompt will appear on the screen asking for the 5 numbers which the utent had seen
+//5. make an array with the numbers inside
+const arrayUtentNumbers = [];
+//6. make a timefunction which waits 3 seconds before to show the prompt to the utent
+setTimeout(function() {
+  //7. make a while cicle to save into an array the utent numbers
   while (arrayUtentNumbers.length < 5) {
     var utentNumber = parseInt(prompt("insert one of the 5 precedent numbers"));
-    if (utentNumber < 1 || utentNumber > 100) {
+    if (utentNumber < min || utentNumber > max) {
       alert("insert a valid number");
     } else if (arrayUtentNumbers.includes(utentNumber)) {
       alert("number already inserted")
@@ -31,3 +32,15 @@ var arrayUtentNumbers = [];
     }
   };
   console.log(arrayUtentNumbers);
+  //8. make a variable for the utent score
+  var score = 0;
+  //9. make a for cicle to find the numbers which the utent had inserted correctly
+  for (var i = 0; i < arrayUtentNumbers.length; i++) {
+    if (arrayNumbers.includes(arrayUtentNumbers[i])) {
+      score += arrayNumbers.includes(arrayUtentNumbers[i]);
+    }
+  };
+  console.log(score);
+  //10. if the numbers are inside of the array, send an alert which says the score of the utent
+  alert("Your score is:" + score);
+}, 3000);
